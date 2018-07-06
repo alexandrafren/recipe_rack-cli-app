23f457e56c5168a7498c616e7ba0c7bef5d5dddc
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-06 19:09:21 +0000

    stub out basic flow

--- reciperack/lib/reciperack/cli.rb
+++ reciperack/lib/reciperack/cli.rb
@@ -2,7 +2,43 @@ class Reciperack::CLI
 
   def call
     puts "Welcome to Recipe Rack!"
+    list_meal_options
+    menu
+    goodbye
   end
 
+  def list_meal_options
+    ["vegan", "vegetarian", "fish", "chicken", "pork", "beef", "random"].each_with_index do |protein, i|
+      puts "#{i + 1}. #{protein}"
+    end
+  end
+
+  def menu
+    puts "What would you like the main of your meal to be? (Type 1-7 or exit)"
+    input = nil
+    while input != "exit"
+    input = gets.strip
+    end
+    case input
+    when "1"
+      vegan meals
+    when "2"
+      vegetarian meals
+    when "3"
+      fish meals
+    when "4"
+      chicken meals
+    when "5"
+      pork meals
+    when "6"
+      beef meals
+    when "7"
+      random meals
+    end
+  end
+
+    def goodbye
+      puts "Enjoy your meal!"
+    end
+
 end
- 
