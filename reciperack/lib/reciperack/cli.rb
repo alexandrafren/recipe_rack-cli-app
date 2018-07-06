f359a1fe8325f3e44ad2579691d7891a3610705c
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-06 22:19:39 +0000

    Adjust logic flow

--- reciperack/lib/reciperack/cli.rb
+++ reciperack/lib/reciperack/cli.rb
@@ -2,46 +2,47 @@ class Reciperack::CLI
 
   def call
     puts "Welcome to Recipe Rack!"
-    list_meal_options
+    list_protein_options
     menu
   end
 
-  def list_meal_options
+  def list_protein_options
     ["vegan", "vegetarian", "fish", "chicken", "pork", "beef", "random"].each_with_index do |protein, i|
       puts "#{i + 1}. #{protein}"
     end
   end
 
   def menu
-    puts "What would you like the main of your meal to be? (Type 1-7 or exit)"
     input = nil
     while input != "exit"
-    input = gets.strip
-    end
-    case input
-    when "1"
-      vegan meals
-    when "2"
-      vegetarian meals
-    when "3"
-      fish meals
-    when "4"
-      chicken meals
-    when "5"
-      pork meals
-    when "6"
-      beef meals
-    when "7"
-      random meals
-    when "exit"
-      puts "Enjoy your meal!"
-    else
-      puts "Error. Choose a number between 1 -7 or type exit."
+      puts "What would you like the main of your meal to be? (Type 1-7 or exit)"
+      input = gets.strip.downcase
+      if input.to_i > 0
+        if input.to_i == 1
+           puts "vegan meals"
+        elsif input.to_i == 2
+           puts "vegetarian meals"
+        elsif input.to_i == 3
+           puts "fish meals"
+        elsif input.to_i == 4
+          puts "chicken meals"
+        elsif input.to_i == 5
+          puts "pork meals"
+        elsif input.to_i == 6
+          puts "beef meals"
+        elsif input.to_i == 7
+          puts "random meals"
+        end
+      elsif input == "exit"
+        self.goodbye
+      else
+        puts "Error. Choose a number between 1 -7 or type exit."
     end
   end
+end
 
-    def goodbye
-      puts "Enjoy your meal!"
-    end
+   def goodbye
+     puts "Enjoy your meal!"
+   end
 
 end
